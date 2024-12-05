94b8b9dd76f4afaf7fa4b58adebd628a0c08fe2c
Poidevin Merdrignac, Francois <francois.poidevin@yahoo.fr>
2024-12-05 08:35:57 +0100

    feat: introduce setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(name='IGNwmtsPyLib',
+      version='1.0',
+      description='Provide a python library for using IGN WMTS web service.',
+      author='Francois Poidevin Merdrignac',
+      author_email='francois.poidevin@yahoo.fr',
+      packages=['wmts'],
+      )
